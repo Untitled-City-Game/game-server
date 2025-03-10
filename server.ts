bd6846f3df745a7e3db776a819a0c86b20b4c261
michael <Michaelturvey97@gmail.com>
2025-03-10 16:05:38 -0400

    firebase frontend

--- server.ts
+++ server.ts
@@ -19,7 +19,7 @@ async function buildServer(){
 	const mapData : GameSetupData = await fetchData();
 	const server = Server({
 		games: [MetroMayhem(mapData.zonePolygons)],
-		origins: [Origins.LOCALHOST, "https://next-metrogame.netlify.app"],
+		origins: [Origins.LOCALHOST, "https://next-metrogame.netlify.app", "https://nextjs-metrogame--metro-game-474bc.us-central1.hosted.app/"],
 		db: new FlatFile({
 			dir: process.cwd() + '/db',
 			logging: false,
